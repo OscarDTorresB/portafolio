fca4aa00be83c02a391239090b6638ee218e5c83
Oscar Torres <oscardtorres@hotmail.com>
2021-11-03 21:02:03 -0500

    Added CEO to contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,19 +1,26 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 
 const Contact: NextPage = () => {
   return (
-    <div className="my-10 mx-auto w-max max-w-full">
-      <h1 className="text-5xl font-bold">
-        I&apos;m still working on this page. 😅
-      </h1>
-      <p>
-        If you&apos;d like to get in touch, please send me an email at{' '}
-        <a href="mailto:oscardtorres@hotmail.com" className="text-blue-400 hover:text-blue-600">
-          my email address
-        </a>
-        .
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Contact me - Oscar&apos;s portfolio</title>
+        <meta name="description" content="Contact me to discuss about software development." />
+      </Head>
+      <div className="my-10 mx-auto w-max max-w-full">
+        <h1 className="text-5xl font-bold">
+          I&apos;m still working on this page. 😅
+        </h1>
+        <p>
+          If you&apos;d like to get in touch, please send me an email at{' '}
+          <a href="mailto:oscardtorres@hotmail.com" className="text-blue-600 hover:text-blue-800">
+            my email address
+          </a>
+          .
+        </p>
+      </div>
+    </>
   );
 }
 

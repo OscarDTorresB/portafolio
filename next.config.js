d0f16e18ac66773a80d6872410dcb0e36373d4e6
Oscar Torres <oscardtorres@hotmail.com>
2021-11-18 22:25:05 -0500

    Used hash for google analytics in CSP

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval'; script-src-elem 'self' www.googletagmanager.com; style-src 'self' fonts.googleapis.com 'unsafe-inline'; font-src 'self' fonts.gstatic.com; img-src 'self' data:; connect-src 'self' vitals.vercel-insights.com"
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval'; script-src-elem 'self' 'sha256-ZbaiDlrQfVgKGZzfa8IH38JRTxTfFXWzuT9b+DbwD2E=' www.googletagmanager.com; style-src 'self' fonts.googleapis.com 'unsafe-inline'; font-src 'self' fonts.gstatic.com; img-src 'self' data:; connect-src 'self' vitals.vercel-insights.com www.google-analytics.com"
           }
         ]
       }
